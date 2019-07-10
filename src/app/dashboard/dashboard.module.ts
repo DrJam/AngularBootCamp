716711e855d2b11b5cfae0be53346c55b58524e5
Dan Ford <dan.ford@yachthavens.com>
2019-07-10 11:23:42 +0100

    layout

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -11,7 +11,12 @@ const dashboardRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent],
+  declarations: [
+    VideoDashboardComponent,
+    VideoListComponent,
+    VideoPlayerComponent,
+    StatFiltersComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild(dashboardRoutes)
