d1bb51e3933c6f77bb3d95d5c56cb41b3610c5ed
Dan Ford <dan.ford@yachthavens.com>
2019-07-10 12:01:44 +0100

    video-list

--- src/app/dashboard/video-list/video-list.component.ts
+++ src/app/dashboard/video-list/video-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { videos } from 'src/app/app.constants';
+import { Video } from 'src/types';
 
 @Component({
   selector: 'app-video-list',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-list.component.scss']
 })
 export class VideoListComponent implements OnInit {
+
+  videos: Video[] = videos;
+
+  getVideoThumbnailUrl = (video: Video) => `https://img.youtube.com/vi/${video.id}/hqdefault.jpg`;
 
   constructor() { }
 
